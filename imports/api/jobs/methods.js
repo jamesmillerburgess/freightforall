80d3f4666c62a38bf0d1bd55a06e7a5914510fe1
JBurgess <jamesmillerburgess@gmail.com>
2016-10-15 22:57:22 +0530

    Re-implemented the add method but with optimistic UI and no loading screen

--- imports/api/jobs/methods.js
+++ imports/api/jobs/methods.js
@@ -3,8 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { Jobs } from './jobs.js';
 
 Meteor.methods({
-  'jobs.addNew'() {
-    const lastJob = Jobs.findOne({}, {sort: { number: -1}});
-    Jobs.insert({number: lastJob.number + 1});
+  'jobs.addNew'(number) {
+    Jobs.insert({number: number});
   }
 });
--- imports/ui/layouts/navbar.js
+++ imports/ui/layouts/navbar.js
@@ -1,10 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+import { Jobs } from '../../api/jobs/jobs.js';
 
 import './navbar.html';
 
 Template.Navbar.events({
   'click #add-job'() {
-    Meteor.call('jobs.addNew')
+    const lastJobNumber = Jobs.findOne({}, {sort: { number: -1}}).number + 1;
+    Meteor.call('jobs.addNew', lastJobNumber, () => FlowRouter.go('/job/' + lastJobNumber));
   }
 });
