f674cf36b9b53d1bf668c03bde0e9e0145635ae7
JBurgess <jamesmillerburgess@gmail.com>
2016-10-15 22:43:21 +0530

    Removed extraneous comments

--- imports/api/jobs/methods.js
+++ imports/api/jobs/methods.js
@@ -4,9 +4,7 @@ import { Jobs } from './jobs.js';
 
 Meteor.methods({
   'jobs.addNew'() {
-    console.log('add new!');
     const lastJob = Jobs.findOne({}, {sort: { number: -1}});
-    console.log(lastJob);
     Jobs.insert({number: lastJob.number + 1});
   }
 });
