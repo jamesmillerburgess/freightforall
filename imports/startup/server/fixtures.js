38a13c03c1dfdee7565aa222588d6c825f0d1b6a
James Burgess <jamesmillerburgess@gmail.com>
2016-11-08 20:58:05 +0100

    Removed database resetting on every server start

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -3,7 +3,7 @@ import { Jobs } from '../../api/jobs/jobs.js';
 
 // if the database is empty on server start, create some sample data.
 Meteor.startup(() => {
-  if (Jobs.find().count() !== -1) {
+  if (Jobs.find().count() === 0) {
     Jobs.remove({});
     Jobs._ensureIndex({'search': 1});
 
