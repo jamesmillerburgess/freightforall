6dbf132f90bed36bb7f1b4f388e714778280994a
James Burgess <jamesmillerburgess@gmail.com>
2016-11-11 09:32:40 +0100

    Migrated package into its own template

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -6,6 +6,7 @@ import '../../ui/layouts/app-body.js';
 import '../../ui/layouts/job-list-item.js';
 import '../../ui/layouts/job-main.js';
 import '../../ui/layouts/cargo-container.js';
+import '../../ui/layouts/cargo-package.js';
 import '../../ui/layouts/navbar.js';
 import '../../ui/pages/root-redirector.js';
 import '../../ui/pages/app-not-found.js';
---
+++ imports/ui/layouts/cargo-package.js
@@ -0,0 +1,2 @@
+import './cargo-package.html';
+
