0e73ad940bfcf4d5cd5f6713b833de8bfbb50f6c
James Burgess <jamesmillerburgess@gmail.com>
2016-11-26 13:56:00 +0100

    Set select value on render

--- imports/ui/layouts/select.js
+++ imports/ui/layouts/select.js
@@ -1,9 +1,14 @@
 import './select.html';
 import './select.less';
 
+Template.Select.onRendered(function onRendered() {
+  let select = this.find('select');
+  select.value = this.data.value;
+});
+
 Template.Select.events({
   'change select'(e) {
     const value = this.options[e.target.selectedIndex] || '';
     Meteor.call('jobs.updateField', this.jobId, this.path, this.field, value);
-  }
+  },
 });
